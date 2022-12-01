18be74300c5e0d99802deed9db15a02207080313
HongheZ <55520646+HongheZ@users.noreply.github.com>
2022-11-30 20:30:54 -0500

    Update SimpleCalculatorTest.java

--- src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
+++ src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
@@ -11,4 +11,10 @@ public class SimpleCalculatorTest {
 		SimpleCalculator calc = new SimpleCalculator();
 		assertEquals(calc.add(1, 1), 2);
 	}
+	public void testMinus() {
+		SimpleCalculator calminus = new SimpleCalculator();
+		assertEquals(calminus.minus(5, 3), 2);
+	}	
 }
+
+
